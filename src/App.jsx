a9a30780157b2c98fec74e6183fe400f1ae9e71f
saarizaws <saad8840@gmail.com>
2024-08-26 23:11:04 +0500

    isDone working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
 
@@ -7,7 +8,7 @@ const App = () => {
   const [todo, setTodo] = useState("");
 
   const handleAdd = () => {
-    setTodos([...todos, {todo, isDone: false}]);
+    setTodos([...todos, {id: uuidv4(),todo, isDone: false}]);
     console.log(todos);
   }
 
@@ -17,6 +18,14 @@ const App = () => {
 
   const handleDelete = () => {
     console.log();
+  }
+
+  const handleIsDone = (e) => {
+    let i = e.target.id;
+    let index = todos.findIndex((item) => item.id === i);
+    let newTodos = [...todos];
+    newTodos[index].isDone = !newTodos[index].isDone;
+    setTodos(newTodos);
   }
 
   return (
@@ -31,13 +40,12 @@ const App = () => {
         <h1 className='text-xl font-bold'>My Todos</h1>
         <div className="todos">
           {todos.map(item => (
-            <>
-            <div className="todo flex">
-            <div className='text'>{item.todo}</div>
+            <div key={item.todo} className="todo flex">
+            <div className={item.isDone ? "line-through" : ""}>{item.todo}</div>
+            <input id={item.id} type="checkbox" value={item.isDone} onChange={handleIsDone}/>  
             <button className='bg-blue-800 hover:bg-blue-950 p-2 py-1 mx-2 text-white rounded-md' onClick={handleEdit}>Edit</button>
             <button className='bg-red-800 hover:bg-red-950 p-2 py-1 mx-2 text-white rounded-md' onClick={handleDelete}>Delete</button>
           </div>
-          </>
           ))}
         </div>
       </div>
