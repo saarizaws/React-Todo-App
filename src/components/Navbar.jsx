90175d7420d770bbb28cd16bde3f9cd9fcacfcca
saarizaws <saad8840@gmail.com>
2024-08-27 16:45:58 +0500

    code cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,6 @@ const Navbar = () => {
       <div className="logo">
         <span className="font-bold text-xl mx-8">iTask</span>
       </div>
-      <ul className="flex gap-8 mx-9">
-        <li className="cursor-pointer hover:font-bold transition-all">Home</li>
-        <li className="cursor-pointer hover:font-bold transition-all">About</li>
-      </ul>
     </nav>
   );
 };
